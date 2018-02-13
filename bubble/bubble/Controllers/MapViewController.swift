e0450ec39c7e1282d20936464d83d4fa11111e4c
sblatz <sblatz@purdue.edu>
2018-02-13 14:52:56 -0500

    Added posting bubble logic

--- bubble/bubble/Controllers/MapViewController.swift
+++ bubble/bubble/Controllers/MapViewController.swift
@@ -20,8 +20,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMap()
-
+        setupBubbleView()
         // Retrieve posts around me with backend function!
+    }
+
+    func setupBubbleView() {
+        createBubbleView.postButton.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControlEvents#>)
     }
 
     // Set up the map view and grab the current location of the user
@@ -57,7 +61,49 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         // TODO: present this with a better animation like coming up from the bottom of the screen
         UIView.animate(withDuration: 1.0) {
             self.createBubbleView.alpha = 1.0
-            // Change alpha level of bubble posting view
         }
     }
+
+    // MARK - Bubble Posting
+
+    func postBubble() {
+        var bubbleData = [String: Any]()
+
+        bubbleData["text"] = createBubbleView.textView.text
+        bubbleData["user"] = "currentUser" // TODO: Use firebase to get FIRUser
+
+        var latitude = 0.0
+        var longitude = 0.0
+
+        if let latitudeCoordinate = locationManager.location?.coordinate.latitude {
+            latitude = Double(latitudeCoordinate)
+        }
+
+        if let longitudeCoordinate = locationManager.location?.coordinate.longitude {
+            longitude = Double(longitudeCoordinate)
+        }
+
+        if createBubbleView.textView.text != "" {
+            DataService.instance.createBubble(bubbleData: bubbleData, latitude: latitude, longitude: longitude, success: { (bubble) in
+                print(bubble)
+            }, failure: { (error) in
+                print(error)
+                let alert = UIAlertController(title: "Error", message: "\(error)", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: {})
+            })
+        } else {
+            let alert = UIAlertController(title: "Cannot post empty bubble", message: "You must enter at least one character to post this bubble.", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: {})
+        }
+    }
+
+    func cancelPost() {
+        createBubbleView.textView.text = ""
+        UIView.animate(withDuration: 1.0) {
+            self.createBubbleView.alpha = 1.0
+        }
+    }
+
 }
