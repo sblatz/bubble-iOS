e4a9421ae58bfdab97912b2648cb6edc03b5500a
Sawyer Blatz <sdblatz@gmail.com>
2018-02-18 18:51:34 -0500

    Post View with Button
    Added a button to post a new bubble in the map view that allows users to post

--- bubble/bubble/Controllers/MapViewController.swift
+++ bubble/bubble/Controllers/MapViewController.swift
@@ -14,14 +14,21 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var createBubbleView: CreateBubbleView!
+    @IBOutlet weak var createBubbleViewCenterY: NSLayoutConstraint!
 
     var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMap()
-
+        setupBubbleView()
         // Retrieve posts around me with backend function!
+    }
+
+    func setupBubbleView() {
+        self.createBubbleViewCenterY.constant = view.frame.height / 2 + createBubbleView.frame.height
+        createBubbleView.postButton.addTarget(self, action: #selector(postBubble), for: .touchUpInside)
+        createBubbleView.cancelButton.addTarget(self, action: #selector(cancelPost), for: .touchUpInside)
     }
 
     // Set up the map view and grab the current location of the user
@@ -54,10 +61,59 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     }
 
     @IBAction func newBubbleButtonPressed(_ sender: Any) {
-        // TODO: present this with a better animation like coming up from the bottom of the screen
-        UIView.animate(withDuration: 1.0) {
-            self.createBubbleView.alpha = 1.0
-            // Change alpha level of bubble posting view
+        createBubbleView.textView.text = ""
+        self.createBubbleViewCenterY.constant = -100
+
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+            self.createBubbleView.alpha = 1
+            self.view.layoutIfNeeded()
+        }) { (finished) in
+            self.createBubbleView.textView.becomeFirstResponder()
+
         }
+    }
+
+    // MARK - Bubble Posting
+
+    @objc func postBubble() {
+        var bubbleData = [String: Any]()
+        var latitude = 0.0
+        var longitude = 0.0
+        bubbleData["text"] = createBubbleView.textView.text
+        bubbleData["user"] = "currentUser" // TODO: Use firebase to get FIRUser
+
+        if let latitudeCoordinate = locationManager.location?.coordinate.latitude {
+            latitude = Double(latitudeCoordinate)
+        }
+
+        if let longitudeCoordinate = locationManager.location?.coordinate.longitude {
+            longitude = Double(longitudeCoordinate)
+        }
+
+        if createBubbleView.textView.text != "" {
+            DataService.instance.createBubble(bubbleData: bubbleData, latitude: latitude, longitude: longitude, success: { (bubble) in
+                print(bubble)
+            }, failure: { (error) in
+                print(error)
+                let alert = UIAlertController(title: "Error", message: "\(error)", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: {})
+            })
+        } else {
+            let alert = UIAlertController(title: "Cannot post empty bubble", message: "You must enter at least one character to post this bubble.", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: {})
+        }
+    }
+
+    @objc func cancelPost() {
+        self.createBubbleViewCenterY.constant = view.frame.height / 2 + createBubbleView.frame.height
+        self.createBubbleView.resignFirstResponder()
+        self.createBubbleView.endEditing(true)
+
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+            self.createBubbleView.alpha = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
